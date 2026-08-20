5f4cb8c9ea5140aecf1dddd56fb01c62e3db6f71
Prashant Tiwari <piyushtiwari71197@gmail.com>
2026-08-20 13:25:23 +0530

    Configure PostgreSQL database

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   "default":{
+       "ENGINE":"django.db.backends.postgresql",
+       "NAME":config("DB_NAME"),
+       "USER":config("DB_USER"),
+       "PASSWORD":config("DB_PASSWORD"),
+       "HOST":config("DB_HOST"),
+       "PORT":config("DB_PORT"),
     }
 }
 
