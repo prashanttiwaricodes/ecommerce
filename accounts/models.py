14f8323d01212938f3f56aad6d1017ba728d8e0d
Prashant Tiwari <piyushtiwari71197@gmail.com>
2026-08-21 19:58:35 +0530

    Built product and Category Management

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,45 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self,email,password=None,**extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+
+        email=self.normalize_email(email)
+
+        user=self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self,email,password=None,**extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+        extra_fields.setdefault("is_active",True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+
+        return self.create_user(
+            email=email,password=password,**extra_fields
+        )
+         
+
+
+
+
 class User(AbstractUser):
     email=models.EmailField(unique=True)
 
     USERNAME_FIELD="email"
     REQUIRED_FIELDS=[]
+
+    objects=UserManager()
