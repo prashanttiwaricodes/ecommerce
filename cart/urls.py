3af67f98d9eecdde0663ad7defd432c7810ba0ad
Prashant Tiwari <piyushtiwari71197@gmail.com>
2026-08-28 12:44:16 +0530

    Add cart UI and views

---
+++ cart/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name="cart"
+
+urlpatterns=[
+    path("", views.cart_detail, name="detail"),
+    path("add/<int:product_id>/", views.add_product_to_cart,name="add"),
+    path("update/<int:product_id>/",views.update_cart,name="update"),
+    path("remove/<int:product_id>/",views.remove_product_from_cart,name="remove"),
+]
