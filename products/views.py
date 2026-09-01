04893de29d39d2344e3cef28fc96ac59a661ef7c
Prashant Tiwari <piyushtiwari71197@gmail.com>
2026-09-01 22:34:24 +0530

    Add product listing and detail

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 # Create your views here.
+def product_list(request):
+    products=Product.objects.filter(is_active=True)
+    
+
+    return render(request,"Product/product_list.html",{"products": products},)
+
+
+def product_detail(request,product_id):
+    product=get_object_or_404(Product,id=product_id,is_active=True)
+    print("PRODUCT:",product)
+
+    return render(request,"Product/product_detail.html",{"product":product},)
